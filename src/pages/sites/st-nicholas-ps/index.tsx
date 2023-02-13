dd4886b73676f30b2671151a880534205561ef52
kpogodev <k.pogorzelski.dev@gmail.com>
2023-02-13 23:05:37 +0000

    adding two dummy themes

---
+++ src/pages/sites/st-nicholas-ps/index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>Welcome to St. Nicholas Primary School Template</h1>
+    </div>
+  )
+}
+
+export default Index
--- src/pages/sites/theme-a.tsx
+++
@@ -1,9 +0,0 @@
-const ThemeA = () => {
-  return (
-    <div>
-      <h1>Theme A tutaj</h1>
-    </div>
-  )
-}
-
-export default ThemeA
