763edb796bac981b9daa9cd317c1f6de7916ac1d
kpogodev <k.pogorzelski.dev@gmail.com>
2023-04-16 00:39:45 +0100

    Cosmetics of handleOnDragEnd function in Navigation List

--- src/components/cms/navigation/NavigationList.tsx
+++ src/components/cms/navigation/NavigationList.tsx
@@ -47,118 +47,131 @@ const NavigationList = () => {
     },
   })
 
+  // Handle Reordering of Pages
+  const handlePageReorder = (result: DropResult) => {
+    if (!result.destination || !result.source) return
+    const items = [...pages]
+    const [reorderedItem] = items.splice(result.source.index, 1)
+    if (!reorderedItem) return
+    items.splice(result.destination.index, 0, reorderedItem)
+    items.forEach((item, index) => {
+      item.order = index
+    })
+    setPages(items)
+    reorderPages(items.map((item) => ({ pageId: item.id, newOrder: item.order })))
+  }
+
+  // Handle Reordering of Subpages
+  const handleSubpageReorder = (result: DropResult) => {
+    if (!result.destination || !result.source) return
+    const pageId = result.source.droppableId.split("-")[2]
+    const page = pages?.find((page) => page.id === pageId)
+
+    if (!page) return
+
+    const items = [...page.children]
+    const [reorderedItem] = items.splice(result.source.index, 1)
+
+    if (!reorderedItem) return
+
+    items.splice(result.destination.index, 0, reorderedItem)
+    items.forEach((item, index) => {
+      item.order = index
+    })
+    const newPages = pages?.map((page) => {
+      if (page.id === pageId) {
+        page.children = items
+      }
+      return page
+    })
+    setPages(newPages)
+    reorderPages(items.map((item) => ({ pageId: item.id, newOrder: item.order })))
+  }
+
+  // Move Subpage to another Page
+  const handleMoveSubpageToAnotherPage = (result: DropResult) => {
+    if (!result.destination || !result.source) return
+    const sourcePageId = result.source.droppableId.split("-")[2]
+    const destinationPageId = result.destination.droppableId.split("-")[2]
+    if (!sourcePageId || !destinationPageId) return
+
+    const sourcePage = pages?.find((page) => page.id === sourcePageId)
+    const destinationPage = pages?.find((page) => page.id === destinationPageId)
+
+    if (!sourcePage || !destinationPage) return
+
+    const sourceItems = [...sourcePage.children]
+    const destinationItems = [...destinationPage.children]
+    const [movedItem] = sourceItems.splice(result.source.index, 1)
+
+    if (!movedItem) return
+
+    const updatedMovedItem = {
+      ...movedItem,
+      parentId: destinationPageId,
+    }
+
+    destinationItems.splice(result.destination.index, 0, updatedMovedItem)
+
+    sourceItems.forEach((item, index) => {
+      item.order = index
+    })
+    destinationItems.forEach((item, index) => {
+      item.order = index
+    })
+
+    const newPages = pages?.map((page) => {
+      if (page.id === sourcePageId) {
+        page.children = sourceItems
+      }
+      if (page.id === destinationPageId) {
+        page.children = destinationItems
+      }
+      return page
+    })
+
+    movePage({ pageId: movedItem.id, newParentId: destinationPageId })
+    setPages(newPages)
+  }
+
+  // Transform Subpage itoto a top level Page
+  const handleTransformIntoPage = (result: DropResult) => {
+    if (!result.destination || !result.source) return
+    const sourcePageId = result.source.droppableId.split("-")[2]
+    const sourcePage = pages?.find((page) => page.id === sourcePageId)
+
+    if (!sourcePage) return
+
+    const sourceItems = [...sourcePage.children]
+    const [movedItem] = sourceItems.splice(result.source.index, 1)
+    if (!movedItem) return
+
+    sourceItems.forEach((item, index) => {
+      item.order = index
+    })
+
+    const newParentChildren = sourceItems.map((item) => {
+      return { pageId: item.id, newOrder: item.order }
+    })
+
+    transformIntoPage({ pageId: movedItem.id, parentChildren: newParentChildren, newOrder: pages.length })
+  }
+
+  // Event Handlers
   const handleOnDragStart = useCallback(() => {
     setIsDragging(true)
   }, [])
 
   const handleOnDragEnd = (result: DropResult) => {
     setIsDragging(false)
-
     if (!result.destination || !result.source) return
 
-    // Handle Reordering of Pages
-    if (result.destination.droppableId === "page-list" && pages) {
-      const items = [...pages]
-      const [reorderedItem] = items.splice(result.source.index, 1)
-      if (!reorderedItem) return
-      items.splice(result.destination.index, 0, reorderedItem)
-      items.forEach((item, index) => {
-        item.order = index
-      })
-      setPages(items)
-      reorderPages(items.map((item) => ({ pageId: item.id, newOrder: item.order })))
-    }
-
-    // Handle Subpage Actions
+    if (result.destination.droppableId === "page-list" && pages) return handlePageReorder(result)
+    
     if (result.source.droppableId.startsWith("subpage-list")) {
-      // Reorder Subpages
-      if (result.source.droppableId === result.destination.droppableId) {
-        const pageId = result.source.droppableId.split("-")[2]
-        const page = pages?.find((page) => page.id === pageId)
-
-        if (!page) return
-
-        const items = [...page.children]
-        const [reorderedItem] = items.splice(result.source.index, 1)
-
-        if (!reorderedItem) return
-
-        items.splice(result.destination.index, 0, reorderedItem)
-        items.forEach((item, index) => {
-          item.order = index
-        })
-        const newPages = pages?.map((page) => {
-          if (page.id === pageId) {
-            page.children = items
-          }
-          return page
-        })
-        setPages(newPages)
-        reorderPages(items.map((item) => ({ pageId: item.id, newOrder: item.order })))
-      } else if (result.destination.droppableId.startsWith("subpage-list")) {
-        // Move Subpage to another Page
-        const sourcePageId = result.source.droppableId.split("-")[2]
-        const destinationPageId = result.destination.droppableId.split("-")[2]
-        if (!sourcePageId || !destinationPageId) return
-
-        const sourcePage = pages?.find((page) => page.id === sourcePageId)
-        const destinationPage = pages?.find((page) => page.id === destinationPageId)
-
-        if (!sourcePage || !destinationPage) return
-
-        const sourceItems = [...sourcePage.children]
-        const destinationItems = [...destinationPage.children]
-        const [movedItem] = sourceItems.splice(result.source.index, 1)
-
-        if (!movedItem) return
-
-        const updatedMovedItem = {
-          ...movedItem,
-          parentId: destinationPageId,
-        }
-
-        destinationItems.splice(result.destination.index, 0, updatedMovedItem)
-
-        sourceItems.forEach((item, index) => {
-          item.order = index
-        })
-        destinationItems.forEach((item, index) => {
-          item.order = index
-        })
-
-        const newPages = pages?.map((page) => {
-          if (page.id === sourcePageId) {
-            page.children = sourceItems
-          }
-          if (page.id === destinationPageId) {
-            page.children = destinationItems
-          }
-          return page
-        })
-
-        movePage({ pageId: movedItem.id, newParentId: destinationPageId })
-        setPages(newPages)
-      } else {
-        // Transform Subpage to become a Page
-        const sourcePageId = result.source.droppableId.split("-")[2]
-        const sourcePage = pages?.find((page) => page.id === sourcePageId)
-
-        if (!sourcePage) return
-
-        const sourceItems = [...sourcePage.children]
-        const [movedItem] = sourceItems.splice(result.source.index, 1)
-        if (!movedItem) return
-
-        sourceItems.forEach((item, index) => {
-          item.order = index
-        })
-
-        const newParentChildren = sourceItems.map((item) => {
-          return { pageId: item.id, newOrder: item.order }
-        })
-
-        transformIntoPage({ pageId: movedItem.id, parentChildren: newParentChildren, newOrder: pages.length })
-      }
+      if (result.source.droppableId === result.destination.droppableId) return handleSubpageReorder(result)
+      if (result.destination.droppableId.startsWith("subpage-list")) return handleMoveSubpageToAnotherPage(result)
+      if (result.destination.droppableId === "pop-subpage") return handleTransformIntoPage(result)
     }
   }
 
